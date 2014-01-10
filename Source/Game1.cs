c3fa101416faaf419f2156a3918b4c584173d843
Daniel Manning <daniel.manning@demandmedia.com>
2014-01-09 18:18:44 -0600

    cleaned up using statements

--- Source/Game1.cs
+++ Source/Game1.cs
@@ -1,16 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Audio;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.GamerServices;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Media;
 using FontBuddyLib;
 using GameTimer;
 using HadoukInput;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace ControllerDirectionTest
 {
